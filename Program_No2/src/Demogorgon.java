efcd43b31750c8d53856fe08ebd9d64bd2771526
Aziev Hanif <115721604+azievhanif@users.noreply.github.com>
2022-12-30 11:11:54 +0700

    Add files via upload

---
+++ Program_No2/src/Demogorgon.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HANIP
+ */
+class Demogorgon extends Enemy {
+    void jump(){
+        System.out.println("Demogorgon melompat!");
+    }
+}
---
+++ Program_No2/src/Enemy.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HANIP
+ */
+class Enemy {
+    String name;
+    int hp;
+    int attackPoin;
+
+    void attack(){
+        System.out.println("Serang!");
+    }
+    void walk(){
+        System.out.println("Jalan!");
+    }
+}
